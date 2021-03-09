5920e4f0a85f96d7428f1f69e2d10d21095b9287
ashhingle <ash.hingle@gmail.com>
2021-03-09 11:13:51 -0500

    Added 1st word dict, formatter, checks for ! and ?

--- AIT590_assignment2.py
+++ AIT590_assignment2.py
@@ -9,6 +9,9 @@ import sys
 import os
 import time
 
+#stores the current time of the request from user as the 'start time'
+start_time = time.time()
+
 #Interprets the number of arguments a user is passing in the command line
 arguments = len(sys.argv)
 
@@ -34,9 +37,22 @@ sentences = int(sys.argv[2])
 min_length = 10
 filenames = []
 
+def generate_first_words():
+    for sent in sentToken:
+        #tokenize te individual sentence and save it in a temp list
+        holder = word_tokenize(sent)
+        seq = ' '.join(holder[i:i+words])
+
+        # If the sentence has atleast the number of ngrams requested, then this sentence can be considered
+        # for use with as a first word
+        if len(holder) > words+1:
+            if  seq not in first_words.keys():
+                first_words[seq] = []
+            first_words[seq].append(holder[words])
+
 def generate_sentence():
     running = True
-    curr_sequence = random.choice(list(ngrams))
+    curr_sequence = random.choice(list(first_words))
     output = curr_sequence
     while running == True:
         if curr_sequence not in ngrams.keys():
@@ -47,13 +63,31 @@ def generate_sentence():
         seq_words = nltk.word_tokenize(output)
         curr_sequence = ' '.join(seq_words[len(seq_words)-words:len(seq_words)])
 
-        if bool(re.search("\.", next_word)) == True:
+        if bool(re.search('[\.\?!]', next_word)) == True:
             running = False
     return output
+
+#format the final output
+def formatter(str):
+    # Change the first letter to a capital letter
+    str = str[0].upper() + str[1:] 
+    
+    # Change the final punctuation to format correctly without an extra space 
+    if str[-1] == '.':
+        str = re.sub(r'( \.)', '.', str)
+    elif str[-1] == '!':
+        str = re.sub(r'( \!)', '!', str)
+    elif str[-1] == '?':
+        str = re.sub(r'( \?)', '?', str)
+        
+    # Captialize i to I in the text
+    str = re.sub(r'( i )', ' I ', str)
+
+    return str
     
 def count_words(str):
     return len(re.findall(r'\w+', str))
-    
+
 for i in range(3, len(sys.argv)):
     filenames.append(str(cwd) + "\\Text Files\\" + sys.argv[i])
 
@@ -63,33 +97,38 @@ with open(fname, "w", encoding='utf-8') as f:
         if index != (len(filenames) - 1):
             f.write(" ")
 
-
 fo = open (fname, "r", encoding= 'utf-8')
 corpus = fo.read()
 corpus = corpus.lower()
-corpus = re.sub(r'[^A-Za-z0-9. ]', ' ', corpus)
+corpus = re.sub(r'[^A-Za-z0-9.\?!\' ]', ' ', corpus)
 wordToken = word_tokenize(corpus)
+sentToken = sent_tokenize(corpus)
 
-#stores the current time of the request from user as the 'start time'
-start_time = time.time()
-
+#initilaize the first words and ngrams dictionaries
+first_words = {}
 ngrams = {}
+
+#creates a list of the first words in the sentences that can be used to generate starting positions
+generate_first_words()
+
 for i in range(len(wordToken)-words):
     seq = ' '.join(wordToken[i:i+words])
     #print(seq[:10])
     if  seq not in ngrams.keys():
         ngrams[seq] = []
     ngrams[seq].append(wordToken[i+words])
-
+    
 for i in range(sentences):
     output = generate_sentence()
     while (count_words(output) < min_length):
         output = generate_sentence()
+    # formats the output before printing
+    output = formatter(output)
     print("Sentence " + str(i + 1) + ": " + output)
 
 #Stores time it took for n-gram model to process input as 'stop time'    
 stop_time = time.time()
 #subtract the two time periods to calculate the runtime of the n-gram:
-print('Time elapsed : ', round(stop_time - start_time), 'secs')
+print('Time elapsed :', round(stop_time - start_time), 'secs')
 
 
